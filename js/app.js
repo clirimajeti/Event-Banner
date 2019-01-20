f77dd1e06b1a13d2b8d442d0c0fac9cebf41c81e
Clirim Ajeti <c.ajeti@intes.co>
2019-01-20 13:06:03 +0100

    AngularJS API call

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module("myApp",[]);
+
+app.filter('htmlToPlaintext', function() {
+    return function(text) {
+      return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
+    };
+  });
+
+app.filter('deCode', function() {
+      return window.decodeURIComponent;
+  });
+
+app.filter('unsafe', function($sce){
+   return $sce.trustAsHtml; });
+
+app.controller("myCtrl",['$scope','$http',function ($scope,$http) {
+      $http.get("https://test-api.jamatu.com/api/events/self/upcoming/Ge7vMUwkSEWmMWOk12uTFg==/ecabae94cade4077b96b900037571607?organizationid=2171&sort=startdate_asc")
+            .then(function(response){
+                $scope.events = response.data;
+            });
+      $scope.clickMe = function(){
+        console.log("WORKING");
+      }
+
+}]);
