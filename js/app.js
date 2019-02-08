2ad5f9045628be2745401e7ca8e7b3d29ffdd5b8
unknown <clirimajeti@gmail.com>
2019-02-08 23:07:32 +0100

    changed to past evets

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ app.filter('unsafe', function($sce){
    return $sce.trustAsHtml; });
 
 app.controller("myCtrl",['$scope','$http',function ($scope,$http) {
-      $http.get("https://test-api.jamatu.com/api/events/self/upcoming/Ge7vMUwkSEWmMWOk12uTFg==/ecabae94cade4077b96b900037571607?organizationid=2171&sort=startdate_asc")
+      $http.get("https://test-api.jamatu.com/api/events/self/past/Ge7vMUwkSEWmMWOk12uTFg==/ecabae94cade4077b96b900037571607?organizationid=2171&sort=enddate_desc")
             .then(function(response){
                 $scope.events = response.data;
             });
