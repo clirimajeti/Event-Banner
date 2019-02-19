563be2095d15f2fc5d6a265ea8cd3cbce441a27c
Çlirim Ajeti <36482517+clirimajeti@users.noreply.github.com>
2019-02-19 01:34:30 +0100

    re-formating the code and added some comments

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 var app = angular.module("myApp",[]);
+const apiUrl = "https://test-api.jamatu.com/api/events/self/past/Ge7vMUwkSEWmMWOk12uTFg==/ecabae94cade4077b96b900037571607?organizationid=2171&sort=enddate_desc";
 
-app.filter('htmlToPlaintext', function() {
-    return function(text) {
+//Making the API call and storing the API data
+app.controller("myCtrl",['$scope','$http',function ($scope,$http) {
+       $http.get(apiUrl).then(function(response){
+            //Storing the data of the response into "events" for using displaying into the AngularJS interpolation
+                $scope.events = response.data;
+            });
+}]);
+
+//For formating the data that is stored in events but also for formating data in other places where needed, I created some filters.
+
+//For security reasons, html should convert to text, so the scripts that could be used will be eliminated.
+//Converter
+app.filter('htmlToPlaintext', function(){
+    return function(text){
       return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
     };
   });
 
+//Decode URI
 app.filter('deCode', function() {
       return window.decodeURIComponent;
   });
 
+//Trust HTML if the description formating is needed
 app.filter('unsafe', function($sce){
-   return $sce.trustAsHtml; });
+   return $sce.trustAsHtml;
+});
 
-app.controller("myCtrl",['$scope','$http',function ($scope,$http) {
-      $http.get("https://test-api.jamatu.com/api/events/self/past/Ge7vMUwkSEWmMWOk12uTFg==/ecabae94cade4077b96b900037571607?organizationid=2171&sort=enddate_desc")
-            .then(function(response){
-                $scope.events = response.data;
-            });
-}]);
+
